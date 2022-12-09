19fbcf8c648d2d70bad0eb454d0e11e69e3e7051
NikitfKorneev <109066903+NikitfKorneev@users.noreply.github.com>
2022-12-09 12:52:56 +0300

    Create mathematic.php

---
+++ mathematic.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+<head >
+    <link rel="stylesheet"  href="style.css">
+    <?php $title = 'Корнеев Никита Владимирович' ?>
+    <title><?php echo $title ?></title>
+    <meta charset="UTF-8">
+</head>
+
+<header >
+<h1 class = "h">Вариант - 4</h1>
+</header>
+<body>
+    <main class="main">
+    <div>
+        <div>
+            <?php include "math.php" ?>
+        </div>
+    </div>        
+    </main>
